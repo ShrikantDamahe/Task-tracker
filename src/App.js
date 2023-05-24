c84aec58fb25395f2c868ff29bf38a6b28d098be
unknown <shrikantdamahe143@hmail.com>
2023-05-24 11:16:03 +0530

    task teracker

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [taskName, setTaskName] = useState('');
+  const [taskDescription, setTaskDescription] = useState('');
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    // Load tasks from localStorage
+    const savedTasks = localStorage.getItem('tasks');
+    if (savedTasks) {
+      setTasks(JSON.parse(savedTasks));
+    }
+  }, []);
+
+  useEffect(() => {
+    // Save tasks to localStorage
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
+
+  const handleTaskNameChange = (e) => {
+    setTaskName(e.target.value);
+  };
+
+  const handleTaskDescriptionChange = (e) => {
+    setTaskDescription(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTask = {
+      id: Date.now(),
+      name: taskName,
+      description: taskDescription,
+    };
+    setTasks([...tasks, newTask]);
+    setTaskName('');
+    setTaskDescription('');
+  };
+
+  const handleDelete = (taskId) => {
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Task Tracker</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="taskName">Task Name:</label>
+          <input
+            type="text"
+            id="taskName"
+            value={taskName}
+            onChange={handleTaskNameChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="taskDescription">Task Description:</label>
+          <input
+            type="text"
+            id="taskDescription"
+            value={taskDescription}
+            onChange={handleTaskDescriptionChange}
+          />
+        </div>
+        <button type="submit">Add Task</button>
+      </form>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            <strong>{task.name}</strong> - {task.description}
+            <button onClick={() => handleDelete(task.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
